292248cb107a0f4c809e0019c480d0550bf9f9ec
Qincheng Sheng <69036207+Qincheng-Sheng@users.noreply.github.com>
2022-01-15 16:50:47 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# src_camera.py
+# client.py
 
 import cv2
 import time
@@ -7,36 +7,33 @@ import socket
 from PIL import Image
 from io import BytesIO
 
-#获取摄像头
+#get camera
 cap = cv2.VideoCapture(0)
-#调整采集图像大小为640*480
+#change picture size 640*480
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 HOST, PORT = '192.168.0.108', 8000
-#连接服务器
+#connect to host
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
 while True:
-  ret, img = cap.read()  #获取一帧图像
-  if ret is False:       #未获取图像时，退出循环
+  ret, img = cap.read()  #get one frame picture
+  if ret is False:       #if get nothing exit
     print("can not get this frame")
     continue
   
   
-  pi = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  #将OpenCV下的图像转换成PIL支持的格式
-  buf = BytesIO()             #缓存对象
-  pi.save(buf, format = 'JPEG')         #将PIL下的图像压缩成jpeg格式，存入内存中的字符串
-  jpeg = buf.getvalue()                 #从buf中读出jpeg格式的图像
+  pi = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  #replace opencv picture format to PIL format
+  buf = BytesIO()             #store in buffer
+  pi.save(buf, format = 'JPEG')         #compress PIL picture to jpeg and write to buffer
+  jpeg = buf.getvalue()                 #read jpeg from buffer
   buf.close()
-  transfer = jpeg
-  print(len(transfer))
-  print(transfer[-1])
-  sock.sendall(transfer)         #通过socket传到服务器
+  sock.sendall(jpeg)         #transmit jpeg to server
 
 '''
-cap.release()                           #释放摄像头
-cv2.destroyAllWindows()                 #关闭所有窗口
+cap.release()                           #close camera
+cv2.destroyAllWindows()                 #close window
 '''
-sock.close()                            #关闭socket
+sock.close()                            #close socket
